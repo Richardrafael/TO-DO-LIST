258951f03d9a2a02dfaa5b58354dab811c03ac3e
richardrafael <richard.rafael.soares@gmail.com>
2025-09-28 20:51:56 -0300

    feat: actions

--- webapp/util/Helper.js
+++ webapp/util/Helper.js
@@ -1,6 +1,18 @@
 sap.ui.define(["require"], (require) => {
 	"use strict";
 	return {
+		formatPriorityClass(TpPriority) {
+			switch (TpPriority) {
+				case 'A':
+					return 'tpPriorityBoxA';
+				case 'M':
+					return 'tpPriorityBoxM';
+				case 'B':
+					return 'tpPriorityBoxB';
+				default:
+					return 'tpPriorityBoxB';
+			}
+		},
 		resolvePath(sPath) {
 			// Relative to application root
 			return require.toUrl("../") + sPath;
